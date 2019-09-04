c93fcf47985cbd59cf394a903213ea4f8032deea
Chase Kallemeyn <chase.kallemeyn@gmail.com>
2019-09-03 20:33:02 -0500

    [add] completes Step-2 solution

--- client/src/components/EventList.jsx
+++ client/src/components/EventList.jsx
@@ -1,12 +1,32 @@
 import React, { Fragment } from 'react'
 
-export default function EventList({ events }) {
+export default function EventList({ events, search, reset }) {
+
+  let filteredEvents = events.filter((e) => (
+    (e.name.includes(search)) ? e : null
+  ));
+
+  // if (filteredEvents.length < 1) {
+  //   setTimeout(() => {
+  //     reset();
+  //   }, 3000)
+  //   return (
+  //     <h4>Event not found. please try again</h4>
+  //   )
+  // }
   return (
     <Fragment>
       <ul>
-        {events.map((event, i) => (
-          <li key={i}>{event.name}</li>
-        ))}
+        {(filteredEvents.length < 1)
+          ? <h4>Event not found.
+              <span>
+                <a href="#" onClick={reset}> please try again </a>
+              </span>
+            </h4>
+          : filteredEvents.map((event, i) => {
+              return <li key={i}>{ event.name }</li>
+        })
+        }
       </ul>
     </Fragment>
   )
