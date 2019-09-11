ab2908bc78542e590a3c65933b653abc2f1c81a0
Chase Kallemeyn <chase.kallemeyn@gmail.com>
2019-09-11 16:26:25 -0500

    [add] starts addEventForm component

---
+++ client/src/components/addEventForm.jsx
@@ -0,0 +1,23 @@
+import React, { Fragment }from 'react'
+
+function addEventForm({ handleInput }) {
+
+  
+  return (
+    <Fragment>
+      <form action="">
+        <label htmlFor=""></label>
+        <input type="text"/>
+        <label htmlFor=""></label>
+        <input type="text"/>
+        <label htmlFor=""></label>
+        <input type="text"/>
+        <label htmlFor=""></label>
+        <input type="text"/>
+        <label htmlFor=""></label>
+        <input type="text"/>
+      </form>
+    </Fragment>
+  )
+export default addEventForm
+
