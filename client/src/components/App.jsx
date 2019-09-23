38aa22213d83881895256752fa37008a0856dc1d
SeungJin Kim <seungjink1@gmail.com>
2019-09-22 20:02:37 -0700

    Step 1 Complete

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,27 @@
 import React, { Component } from 'react'
+import EventList from './Eventlist.jsx';
 
 export default class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      events: null,
+      events: [
+        {name: 'AngularConnect'},
+        {name: 'DevFest'},
+        {name: 'JSConf'},
+        {name: 'VueConf'},
+        {name: 'ReactConf'},
+      ],
     }
   }
 
   render() {
+    const {events} = this.state;
     return (
       <div>
         <h1>EventStack</h1>
+        <EventList events={events} />
       </div>
     )
   }
---
+++ client/src/components/EventList.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+export default function EventList ( { events }) {
+  return (
+    <Fragment>
+      <ul>
+        {events.map((event, i) => (
+          <li key ={i}>{event.name}</li>
+        ))}
+      </ul>
+    </Fragment>
+  )
+}
