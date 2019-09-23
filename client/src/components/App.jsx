4cb05c7e641aaa678853afcffcc016cc02a017ff
SeungJin Kim <seungjink1@gmail.com>
2019-09-23 01:02:35 -0700

    Step 2 Complete

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -13,15 +13,76 @@ export default class App extends Component {
         {name: 'VueConf'},
         {name: 'ReactConf'},
       ],
+      searchEvents: '',
+      search: '',
+      submitted: false,
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.resetState = this.resetState.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const app = this;
+
+    if (app.state.searchEvents !== '') {
+      this.setState({
+        submitted: true,
+        searchEvents: '',
+      });
     }
   }
 
+  resetState(e) {
+    if (e) {
+      e.preventDefault();
+    }
+
+    this.setState({
+      submitted: false,
+      searchEvents: '',
+      search: ''
+    })
+  }
+
+  handleSearch(e) {
+    const {name, value} = e.target;
+    console.log('e target', e.target);
+    if (value.length < 1) {
+      this.resetState();
+    } else {
+      this.setState({
+        [name]: value,
+        search: value,
+      })
+    }
+
+  }
+
   render() {
-    const {events} = this.state;
+    const {events, search, searchEvents, submitted} = this.state;
     return (
       <div>
         <h1>EventStack</h1>
-        <EventList events={events} />
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="searchEvents">Search Events </label>
+          <input id="searchEvents" type="text"
+            name= "searchEvents"
+            value={searchEvents}
+            onChange={this.handleSearch}
+          />
+          <input type="submit" value="submit" />
+        </form>
+        {(submitted)
+          ? <EventList
+              events={events}
+              search={search}
+              reset={this.resetState}
+            />
+          : null
+        }
       </div>
     )
   }
--- client/src/components/EventList.jsx
+++ client/src/components/EventList.jsx
@@ -1,13 +1,24 @@
 import React, { Fragment } from 'react'
 
-export default function EventList ( { events }) {
+export default function EventList ( { events, search, reset }) {
+
+  let filteredEvents = events.filter((e) =>
+    (e.name.includes(search)) ? e : null
+  );
+
   return (
     <Fragment>
-      <ul>
-        {events.map((event, i) => (
-          <li key ={i}>{event.name}</li>
-        ))}
-      </ul>
+      {
+        (filteredEvents.length < 1)
+          ? <h4>Event not found.
+            <span>
+              <a href="#" onClick={reset}> please try again</a>
+            </span>
+          </h4>
+          : filteredEvents.map((event, i) => {
+            return <li key={i}>{event.name}</li>
+          })
+      }
     </Fragment>
   )
 }
